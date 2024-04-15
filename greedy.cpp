036e42a66e44d70c544393de6c7db6e088e647c7
Lucas Bitu <lucasbitu55@gmail.com>
2024-04-15 20:26:32 -0300

    MergeSort

--- greedy.cpp
+++ greedy.cpp
@@ -1,46 +1,47 @@
 #include "greedy.h"
 #include <iostream>
-
-using namespace std;
-/*
-#include "greedy.h"
-#include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-void mergeSort(vector<int> &arrayB, &p, &r){
-    int q;
-    if(p<r){
-        q = static_cast<int>(floor((p+r)/2));
-        mergeSort(arrayB, p, r);
-        mergeSort(arrayB, q+1, r);
-        merge(arrayB, p, q, r)
+void mergeInt(vector<int>& v, int i, int meio, int j, int fim, int k, vector<int>& aux) {
+    if (i <= meio && (j > fim || v[i] < v[j])) {
+        aux[k] = v[i];
+        mergeInt(v, i + 1, meio, j, fim, k + 1, aux);
+    } else if (j <= fim) {
+        aux[k] = v[j];
+        mergeInt(v, i, meio, j + 1, fim, k + 1, aux);
     }
 }
 
-void mergeSort(vector<int> &arrayB, &p, &r){
-    int q;
-    if(p<r){
-        q = static_cast<int>(floor((p+r)/2));
-        mergeSort(arrayB, p, r);
-        mergeSort(arrayB, q+1, r);
-        merge(arrayB, p, q, r)
+void mergeSortInt(vector<int>& v, int inicio, int fim, vector<int>& aux) {
+    if (inicio < fim) {
+        int meio = (inicio + fim) / 2;
+        mergeSortInt(v, inicio, meio, aux);
+        mergeSortInt(v, meio + 1, fim, aux);
+        mergeInt(v, inicio, meio, meio + 1, fim, 0, aux);
+
+        for (int l = 0; l <= fim - inicio; ++l) {
+            v[inicio + l] = aux[l];
+        }
     }
 }
 
-int main ()
-{
-  printf ("Hello World");
-
-  return 0;
+void mergeSort(vector<int>& v) {
+    vector<int> aux(v.size());
+    mergeSortInt(v, 0, v.size() - 1, aux);
 }
 
-    
-}
+// int main() {
+//     vector<int> a = {10, 2, 7, 1, 4, 9, 3, 8, 0, 5, 6};
+//     mergeSort(a);
+//     for (int num : a) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
 
-*/
 vector<vector<int>> greedy(int &n, int &m, int &p, std::vector<int> &arrayB, std::vector<std::vector<int>> &matrizT, std::vector<std::vector<int>> &matrizC){
 
     for i 
